8482185f8c0a5156a489dc6d9aa85c829c75ff2e
reRhythm-01 <sevvalery888@gmail.com>
2025-04-14 14:59:04 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,16 +1,18 @@
-+Zadacha
-import nath
-pi - math-pi
-2ー2円1
-while x < 2 pi:
-=×≤D1/4：
-0 = math.cos (x)
-print C
-else:
-• = math. sin (x)
-print ("
-print ("0= "
-print （"x=
-round (Q, 2) ) round (x, 2) )
-×+= D1/12
-print l
+#Zadacha
+import math
+
+pi = math.pi
+x = -2*pi
+Q = 0
+
+while x < 2*pi:
+    if x < pi/4:
+        Q = math.cos(x)
+        print("----------")
+    else:
+        Q = math.sin(x)
+        print("----------")
+    print("Q= ", round(Q,2))
+    print("x= ", round(x,2))
+    x += pi/12
+print("==============================")
